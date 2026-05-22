7b429a8edef61539f098ca5b2dfe0af0d945be79
JohannesRgnr <johannes.regnier@proton.me>
2026-05-22 12:02:33 +0200

    feat: add presets enum

--- Core/Inc/spectra.h
+++ Core/Inc/spectra.h
@@ -12,6 +12,17 @@
 
 #define NUMPRESETS  7
 
+typedef enum presets
+{
+    Bell1,
+    Bell2,
+    Gong,
+    Chord,
+    Saw,
+    Square,
+    CB808
+} presets_t;
+
 extern const float Bell1Partials[];     // Bell 1 (Default Left)
 extern const float Bell2Partials[];     // Bell 2
 extern const float GongPartials[];      // Gong or Cymbal
